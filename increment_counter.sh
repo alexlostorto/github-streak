798768ef2ee184b9f317c34f37eacd1de107cee6
Alex lo Storto <lostorto.business@gmail.com>
2023-07-24 18:40:56 +0100

    Add bash script for automatic streak

---
+++ increment_counter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define the file name and path
+counter_file="counter.txt"
+
+# Check if the counter file exists, if not, create it and initialise the counter to 0
+if [ ! -f "$counter_file" ]; then
+	echo "0" > "$counter_file"
+fi
+
+# Read the current value of the counter from the file
+counter=$(<counter.txt) 
+
+# Increment the counter
+((counter++))
+
+# Update the counter in the file
+echo "$counter" > "$counter_file"
+
+# Display the updated counter value
+echo "Counter incremented. New value: $counter"
+
+# Stage and commit changes to Git
+git add "$counter_file"
+git commit -m "Counter incremented. New value: $counter"
+git push
